4f1e84e96cccfcebbca596abb7b53018510d1f93
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-10-31 09:32:28 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/g46todoapi/G46TodoApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.g46todoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G46TodoApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(G46TodoApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/se/lexicon/g46todoapi/G46TodoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.g46todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G46TodoApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
