80d392f1faf75789acff102ef76dd9082835d48a
Mehrdad Javan <mehrdad.javan@adapta.se>
2023-11-06 11:05:01 +0100

    add todo

---
+++ src/main/java/se/lexicon/g46todoapi/controller/PeopleController.java
@@ -0,0 +1,5 @@
+package se.lexicon.g46todoapi.controller;
+
+public class PeopleController {
+  // todo: implement the controller
+}
---
+++ src/main/java/se/lexicon/g46todoapi/controller/TaskController.java
@@ -0,0 +1,5 @@
+package se.lexicon.g46todoapi.controller;
+
+public class TaskController {
+  // todo: implement the controller
+}
